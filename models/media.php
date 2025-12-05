1f10d7fc12ceffcc061b6654c13f1c3138d4bdb3
Thomas <robindesbois.loulou@gmail.com>
2025-12-05 11:42:58 +0100

    modif fichier media.php

---
+++ models/media.php
@@ -0,0 +1,32 @@
+<?php 
+class media {
+    private ?int $id = NULL;
+    private ?string $url = NULL;
+    private ?string $alt = NULL;
+
+    public function __construct()
+    {
+    }
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+    public function setId (?int $id): void {
+        $this->id = $id;
+    }
+
+    public function getUrl(): ?string {
+        return $this->url;
+    }
+    public function setUrl (?string $url): void {
+        $this->url = $url;
+    }
+
+    public function getAlt(): ?string {
+        return $this->alt;
+    }
+    public function setAlt (?string $alt): void {
+        $this->alt = $alt;
+    }
+}
+?>
