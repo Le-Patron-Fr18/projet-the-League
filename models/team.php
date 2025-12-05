eba8c2f5c45c8b675f29eb52dedbd6870decfd59
Charbelakplogan2002 <acharbel02@gmail.com>
2025-12-05 14:25:53 +0000

    Modif team.php correction

--- models/team.php
+++ models/team.php
@@ -1,44 +1,43 @@
 <?php 
-class team {
-    private ?int $id = NULL;
-    private ?string $name = NULL;
-    private ?string $description  = NULL;
-    private ?string $logo = NULL;
+class Team {
+    private ?int $id = null;
+    private ?string $name = null;
+    private ?string $description = null;
+    private ?int $logo = null;  
+
     public function __construct()
     {
     }
 
+   
     public function getId(): ?int {
         return $this->id;
     }
-    public function setId (?int $id): void {
+    public function setId(?int $id): void {
         $this->id = $id;
     }
+
     
-    public function getname() : ?string {
+    public function getName(): ?string {
         return $this->name;
     }
-
-    public function setname(?string $name) : void {
-<<<<<<< HEAD
+    public function setName(?string $name): void {
         $this->name = $name;
-=======
-        $this->$name = $name;
->>>>>>> f63083c23b13cf188453dbca1d1d01f33c1c7056
     }
-    public function getdescription(): ?string {
+
+    
+    public function getDescription(): ?string {
         return $this->description;
     }
-    public function setdescription(?string $description): void {
-        $this->$description = $description;
+    public function setDescription(?string $description): void {
+        $this->description = $description;
     }
 
-    public function getlogo(): ?string {
+    public function getLogo(): ?int {
         return $this->logo;
     }
-    public function setlogo (?string $logo):  void {
-        $this->$logo = $logo;
+    public function setLogo(?int $logo): void {
+        $this->logo = $logo; 
     }
-
 }
 ?>
