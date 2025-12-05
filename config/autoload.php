4f70909b95a91576811f3ebb242ad0093ec73523
Louis Bossoutrot <louis.bossoutrot.37@gmail.com>
2025-12-05 16:44:31 +0100

    autoload added

---
+++ config/autoload.php
@@ -0,0 +1,16 @@
+<?php
+require "models/game.php";
+require "models/player_performance.php";
+require "models/player.php";
+require "models/team.php";
+
+require "managers/AbstractManager.php";
+require "managers/GameManager.php";
+require "managers/PlayerManager.php";
+require "managers/PlayerPerformancesManager.php";
+require "managers/TeamManager.php";
+
+require "controllers/AbstractController.php";
+require "controllers/DefaultController.php";
+
+require "services/Router.php";
