ab69a78734999cf059e9d138f4f7e055fe7ce998
Louis Bossoutrot <louis.bossoutrot.37@gmail.com>
2025-12-06 10:41:00 +0100

    add PageController

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -2,8 +2,26 @@
 
 class PageController extends AbstractController
 {
-    public function index() : void
+    public function home() : void
     {
-        $this->render("index", []);
+        $teamMan = new TeamManager;
+        $playerMan = new PlayerManager;
+        $gameMan = new GameManager;
+        $data = [
+            "team" => $teamMan->findOne(1),
+            "players" => $playerMan->findAll(1),
+            "highlighted" => [
+                $playerMan->findOne(3),
+                $playerMan->findOne(14),
+                $playerMan->findOne(12),
+            ],
+            "lastMatch" => [
+                "game" => $game = $gameMan -> findLastDate(),
+                "team_1" => $team_1 = $teamMan->findOne($game->getTeam1()),
+                "team_2" => $team_2 = $teamMan->findOne($game->getTeam2()),
+                "winner" => $winner = $teamMan->findOne($game->getWinner())
+            ],
+        ];
+        $this->render("home", $data);
     }
 }
