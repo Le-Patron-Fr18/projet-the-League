0275c84a3a69ad9975bf5d4ac608e83f3c34a783
Thomas <robindesbois.loulou@gmail.com>
2025-12-07 09:37:28 +0100

    ajout fichier player + playerS

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -24,4 +24,51 @@ class PageController extends AbstractController
         ];
         $this->render("home", $data);
     }
+
+    public function player() : void
+    {
+        $teamMan = new TeamManager;
+        $playerMan = new PlayerManager;
+        $gameMan = new GameManager;
+        $data = [
+            "team" => $teamMan->findOne(1),
+            "players" => $playerMan->findAllFromTeam(1),
+            "highlighted" => [
+                $playerMan->findOne(3),
+                $playerMan->findOne(14),
+                $playerMan->findOne(12),
+            ],
+            "lastMatch" => [
+                "game" => $game = $gameMan -> findLastDate(),
+                "team_1" => $team_1 = $teamMan->findOne($game->getTeam1()),
+                "team_2" => $team_2 = $teamMan->findOne($game->getTeam2()),
+                "winner" => $winner = $teamMan->findOne($game->getWinner())
+            ],
+        ];
+        $this->render("player", $data);
+    }
+
+    public function playerS() : void
+    {
+        $teamMan = new TeamManager;
+        $playerMan = new PlayerManager;
+        $gameMan = new GameManager;
+        $data = [
+            "team" => $teamMan->findOne(1),
+            "players" => $playerMan->findAllFromTeam(1),
+            "highlighted" => [
+                $playerMan->findOne(3),
+                $playerMan->findOne(14),
+                $playerMan->findOne(12),
+            ],
+            "lastMatch" => [
+                "nickname" => $nickname = $playerMan -> findLastDate(),
+                "team" => $team = $playerMan->findOne($playerMan->getTeam()),
+                "name" => $name = $teamMan->findOne($teamMan->getId()),
+                
+                
+            ],
+        ];
+        $this->render("playerS", $data);
+    }
 }
