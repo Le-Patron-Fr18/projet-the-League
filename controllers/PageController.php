cc9ec66fcc4ee4726926a8ba12cb8549ea5435ac
Thomas <robindesbois.loulou@gmail.com>
2025-12-07 10:17:28 +0100

    modif fichier

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -27,22 +27,13 @@ class PageController extends AbstractController
 
     public function player() : void
     {
-        $teamMan = new TeamManager;
-        $playerMan = new PlayerManager;
-        $gameMan = new GameManager;
+        $playerperformancesMan = new PlayerManager;
         $data = [
-            "team" => $teamMan->findOne(1),
-            "players" => $playerMan->findAllFromTeam(1),
-            "highlighted" => [
-                $playerMan->findOne(3),
-                $playerMan->findOne(14),
-                $playerMan->findOne(12),
-            ],
-            "lastMatch" => [
-                "game" => $game = $gameMan -> findLastDate(),
-                "team_1" => $team_1 = $teamMan->findOne($game->getTeam1()),
-                "team_2" => $team_2 = $teamMan->findOne($game->getTeam2()),
-                "winner" => $winner = $teamMan->findOne($game->getWinner())
+            "players" => $playerperformancesMan->findAllFromTeam(1),
+            "performances" => [
+                "playerperformances" => $playerperformances = $playerperformancesMan->findAll(),
+                "points" => $points = $playerperformancesMan->findOne(1),
+                "assists" => $assists = $playerperformancesMan->findOne(1),
             ],
         ];
         $this->render("player", $data);
@@ -52,19 +43,14 @@ class PageController extends AbstractController
     {
         $teamMan = new TeamManager;
         $playerMan = new PlayerManager;
-        $gameMan = new GameManager;
+
         $data = [
             "team" => $teamMan->findOne(1),
             "players" => $playerMan->findAllFromTeam(1),
-            "highlighted" => [
-                $playerMan->findOne(3),
-                $playerMan->findOne(14),
-                $playerMan->findOne(12),
-            ],
-            "lastMatch" => [
-                "nickname" => $nickname = $playerMan -> findLastDate(),
-                "team" => $team = $playerMan->findOne($playerMan->getTeam()),
-                "name" => $name = $teamMan->findOne($teamMan->getId()),
+            "playerlist" => [
+                "nickname" => $nickname = $playerMan->findOne(1),
+                "team" => $team = $teamMan->findOne($nickname->getTeam()),
+                "name" => $name = $team->getName(),
                 
                 
             ],
